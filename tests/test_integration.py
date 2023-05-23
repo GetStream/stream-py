d6174698ac3bc8f1700998326fe938443eb5b102
sachaarbonel <sacha.arbonel@hotmail.fr>
2023-05-23 09:36:07 -0400

    integration test wip

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,9 +1,7 @@
 
 import os
 import pytest
-
-from getstream.video.sync.client import VideoClient
-
+from getstream import Stream
 
 VIDEO_API_KEY = os.environ.get("VIDEO_API_KEY")
 BASE_URL = "https://video.stream-io-api.com/video"
@@ -12,7 +10,12 @@ TIMEOUT = 6
 
 @pytest.fixture(scope="module")
 def client():
-    return VideoClient(VIDEO_API_KEY, BASE_URL, TIMEOUT)
+    return Stream(
+    api_key=VIDEO_API_KEY,
+    api_secret=VIDEO_API_SECRET,
+    timeout=TIMEOUT,
+    video_base_url=BASE_URL,
+    )
 
 def test_video_client_initialization(client):
     assert client.api_key == VIDEO_API_KEY
