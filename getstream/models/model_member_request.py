83d894bacbc054737583a9109f993a31f78e1203
sachaarbonel <sacha.arbonel@hotmail.fr>
2023-08-25 16:52:49 +0100

    more lints

--- getstream/models/model_member_request.py
+++ getstream/models/model_member_request.py
@@ -4,6 +4,6 @@ from typing import Optional, Dict, Any
 
 @dataclass
 class MemberRequest:
+    user_id: str
     custom: Optional[Dict[str, Any]] = None
     role: Optional[str] = None
-    user_id: str
