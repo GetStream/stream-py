2a442cedf70afa8a08c3386f4692762c6bbd1d07
sachaarbonel <sacha.arbonel@hotmail.fr>
2023-08-27 14:55:25 +0200

    handle none

--- getstream/models/model_egress_hls_response.py
+++ getstream/models/model_egress_hls_response.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass
+from typing import Optional
 
 
 @dataclass
 class EgressHLSResponse:
-    playlist_url: str
+    playlist_url: Optional[str] = None
 
     @classmethod
     def from_dict(cls, data: dict) -> "EgressHLSResponse":
