5e552ffd349fb7181f158d21481a6d2044d7c691
sachaarbonel <sacha.arbonel@hotmail.fr>
2023-09-18 09:21:08 +0200

    fix

--- getstream/models/api_error.py
+++ getstream/models/api_error.py
@@ -1,12 +1,12 @@
 from dataclasses import dataclass, field
-from typing import Optional
+from typing import List, Optional
 from dataclasses_json import config, dataclass_json
 
 
 @dataclass_json
 @dataclass
 class Apierror:
-    details: list[int] = field(metadata=config(field_name="details"))
+    details: List[int] = field(metadata=config(field_name="details"))
     duration: str = field(metadata=config(field_name="duration"))
     message: str = field(metadata=config(field_name="message"))
     more_info: str = field(metadata=config(field_name="more_info"))
