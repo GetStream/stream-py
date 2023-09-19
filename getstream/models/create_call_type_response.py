93c9fe00818b7432eb434a89f33834b2dde5db75
sachaarbonel <sacha.arbonel@hotmail.fr>
2023-09-19 09:43:42 +0200

    fix?

--- getstream/models/create_call_type_response.py
+++ getstream/models/create_call_type_response.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 from dataclasses_json import config, dataclass_json
-
+from marshmallow import fields
 from typing import List, Dict
 from datetime import datetime
 from getstream.models.call_settings_response import CallSettingsResponse
@@ -14,8 +14,22 @@ class CreateCallTypeResponse:
         metadata=config(field_name="notification_settings")
     )
     settings: CallSettingsResponse = field(metadata=config(field_name="settings"))
-    updated_at: datetime = field(metadata=config(field_name="updated_at"))
-    created_at: datetime = field(metadata=config(field_name="created_at"))
+    updated_at: datetime = field(
+        metadata=config(
+            field_name="updated_at",
+            encoder=datetime.isoformat,
+            decoder=datetime.fromisoformat,
+            mm_field=fields.DateTime(format="iso"),
+        )
+    )
+    created_at: datetime = field(
+        metadata=config(
+            field_name="created_at",
+            encoder=datetime.isoformat,
+            decoder=datetime.fromisoformat,
+            mm_field=fields.DateTime(format="iso"),
+        )
+    )
     duration: str = field(metadata=config(field_name="duration"))
     grants: Dict[str, List[str]] = field(metadata=config(field_name="grants"))
     name: str = field(metadata=config(field_name="name"))
