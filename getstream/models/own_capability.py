0dbae2c910e6ab7feddefdc13dc442755540655c
sachaarbonel <sacha.arbonel@hotmail.fr>
2024-02-05 18:41:05 +0530

    remove classmethod on to_str

--- getstream/models/own_capability.py
+++ getstream/models/own_capability.py
@@ -35,6 +35,5 @@ class OwnCapability(Enum):
         except ValueError:
             raise ValueError(f"'{value}' is not a valid OwnCapability")
 
-    @classmethod
     def to_str(self) -> str:
         return self.value
